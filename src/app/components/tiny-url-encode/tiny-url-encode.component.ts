38facb45a2e5bc849a5cd029802885ef25dc15a0
Alex Coronell <alexcoronell@gmail.com>
2024-12-26 15:53:41 -0500

    Basic properties added on encode component

--- src/app/components/tiny-url-encode/tiny-url-encode.component.ts
+++ src/app/components/tiny-url-encode/tiny-url-encode.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, signal, inject } from '@angular/core';
+import type { FormGroup } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+} from "@angular/forms";
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+/* Types */
+import type { RequestStatus } from '../../types/RequestStatus.type';
+/* Helpers */
+import { MyErrorStateMatcher } from '../../helpers/MyErrorStateMatcher.helper';
+import { urlValidator } from '../../helpers/urlValidator';
 
 @Component({
   selector: 'app-tiny-url-encode',
-  imports: [MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
   templateUrl: './tiny-url-encode.component.html',
   styleUrl: './tiny-url-encode.component.css'
 })
 export class TinyUrlEncodeComponent {
+  /****************************************** Services ******************************************/
+  private formBuilder = inject(FormBuilder);
+
+  /****************************************** Signals ******************************************/
+  requestStatus = signal<RequestStatus>("init");
+
+  /****************************************** Properties ******************************************/
+  form!: FormGroup;
+  errorNameFormControl = new MyErrorStateMatcher();
+
+  /****************************************** Contructor ******************************************/
+  constructor() {
+    this.buildForm();
+  }
+
+  /****************************************** Methods ******************************************/
+  /****** Build Form ******/
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      urlFormControl: ["", [Validators.required, Validators.minLength(10), Validators.maxLength(250), urlValidator]],
+    });
+  }
+
+  /****** onSubmit ******/
+  onSubmit() {
+    if (!this.form.valid) return;
+    this.requestStatus.set("loading");
+    return
+  }
 
 }
