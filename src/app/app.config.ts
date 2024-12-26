3f1ef0f1da6e256627c8b6080a4f2b3ff37fc83c
Alex Coronell <alexcoronell@gmail.com>
2024-12-26 15:02:02 -0500

    Angular material installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 import type { ApplicationConfig } from '@angular/core';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(), provideAnimationsAsync()]
 };
