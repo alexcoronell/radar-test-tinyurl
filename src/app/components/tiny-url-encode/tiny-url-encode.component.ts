6fb97977dacf32849f94928ea9174d3ce5d595af
Alex Coronell <alexcoronell@gmail.com>
2024-12-26 15:29:05 -0500

    TinyUrlEncode component basic layout

--- src/app/components/tiny-url-encode/tiny-url-encode.component.ts
+++ src/app/components/tiny-url-encode/tiny-url-encode.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-tiny-url-encode',
-  imports: [],
+  imports: [MatInputModule, MatButtonModule],
   templateUrl: './tiny-url-encode.component.html',
   styleUrl: './tiny-url-encode.component.css'
 })
