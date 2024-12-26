6c20fad192f0b4639344460a144ca0f507429979
Alex Coronell <alexcoronell@gmail.com>
2024-12-26 15:00:45 -0500

    TinyUrl component created, html provider added, router removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TinyUrlComponent } from './components/tiny-url/tiny-url.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TinyUrlComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/tiny-url/tiny-url.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tiny-url',
+  imports: [],
+  templateUrl: './tiny-url.component.html',
+  styleUrl: './tiny-url.component.css'
+})
+export class TinyUrlComponent {
+
+}
